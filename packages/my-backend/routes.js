52b5ed4c5cd665025adb885823edf68119f26f4a
NickDone <651309599@qq.com>
2022-02-13 14:24:08 +0800

    add 1 line

--- packages/my-backend/routes.js
+++ packages/my-backend/routes.js
@@ -1,7 +1,7 @@
 'use strict'
 const simple = require('./handlers/simple')
 const configured = require('./handlers/configured')
-
+console.log(2);
 module.exports = function (app, opts) {
   // Setup routes, middleware, and handlers
   app.get('/', simple)
